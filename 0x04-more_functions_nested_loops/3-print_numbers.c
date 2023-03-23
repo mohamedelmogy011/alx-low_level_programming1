e9af3f90a7164be6c4307a771518fb9e7891a9c3
mohamedelmogy011 <mohamedelmogy011@gmail.com>
2023-03-23 12:47:12 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - Print the number since 0 up to 0
+ * Return: The number since 0 up to 0
+ */
+
+void print_number(void)
+
+{
+	int x;
+
+	for (x =0; x<= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+
+}
+
